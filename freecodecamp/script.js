fd9b32510fe592164f4fd856ab8aa38a616b99b7
Богдан Атаманюк <bogdanatamanuk@MacBook-Air-Bogdan.local>
2022-09-17 13:06:40 +0200

    add freecodecamp array

--- freecodecamp/script.js
+++ freecodecamp/script.js
@@ -66,3 +66,116 @@ const testObj = {
 const playerNumber = 16; // Змініть цей рядок
 const player = String(testObj[playerNumber]); // Змініть цей рядо
 console.log(player);
+
+//4
+// Налаштування
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.name = 'Happy Coder';
+// Змініть код лише під цим рядком
+console.log(myDog);
+
+//5
+const myDogs = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.bark = 'hav';
+
+//6
+const myDogss = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Змініть код лише під цим рядком
+delete myDog.tails;
+
+//7
+// Налаштування
+function phoneticLookup(val) {
+  let result = "";
+
+  // Змініть код лише під цим рядком
+
+  const lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  };
+   result = lookup[val];
+
+  // Змініть код лише над цим рядком
+  return result;
+}
+console.log(phoneticLookup("charlie"));
+
+//8
+function checkObj(obj, checkProp) {
+  // Змініть код лише під цим рядком
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } 
+  return "Not found";
+  // Змініть код лише над цим рядком
+}
+//9
+function updateRecords(records, id, prop, value) {
+  if (prop !== 'tracks' && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+    records[id][prop] = [value];
+  } else if (prop === "tracks" && value !== "") {
+    records[id][prop].push(value);
+  } else if (value === "") {
+    delete records[id][prop];
+  }
+  return records;
+}
+
+//10
+const myArray = [];
+let i = 5;
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+ 
+}
+console.log(myArray);
+
+//11
+// Налаштування
+const myArr = [];
+for (let i = 1; i < 5; i++) {
+  myArr.push(i);
+}
+
+console.log(myArr);
+
+//12
+// Налаштування
+const myAr = [];
+for (let i = 9; i > 0; i -= 2) {
+  myAr.push(i)
+}
+// Змініть код лише під цим рядком
+
+//13
+const myA = [2, 3, 4, 5, 6];
+let total = 0;
+for (let i = 0; i < myA.length; i++) {
+  total += myA[i];
+}
+console.log(total);
