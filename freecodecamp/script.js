8a006a9eaa6ab44dbdba6d540c0a731fc5fa10de
Богдан Атаманюк <bogdanatamanuk@MacBook-Air-Bogdan.local>
2022-09-17 11:24:44 +0200

    freecodecamp task card

---
+++ freecodecamp/script.js
@@ -0,0 +1,46 @@
+//1 
+
+let count = 0;
+
+function cc(card) {
+  // Змініть код лише під цим рядком
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count -= 1;
+      break;
+  }
+  if (count > 0) {
+    return `${count} Bet`;
+  } else if (count <= 0) {
+    return `${count} Hold`;
+  } else {
+
+    return "Change Me";
+  }
+
+  // Змініть код лише над цим рядком
+}
+
+cc(2);
+cc(3);
+cc(7);
+cc('K');
+cc('A');
+
+//2
