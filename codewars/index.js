36b65faf89ceffbd845d9dbe63c7adf46c43b81e
Богдан Атаманюк <bogdanatamanuk@MacBook-Air-Bogdan.local>
2022-09-17 19:29:05 +0200

    disemvowel task

--- codewars/index.js
+++ codewars/index.js
@@ -19,3 +19,26 @@ function highAndLow(numbers) {
   var arr = numbers.split(" ");
   return Math.max.apply(null, arr) + ' ' + Math.min.apply(null, arr);
 }
+
+//7 kyu Disemvowel Trolls
+//#1
+function disemvowel(str) {
+  let newStr = "";
+for(let i = 0; i < str.length; i++){
+ if ("aeiou".includes(str[i].toLowerCase())) continue;
+newStr += str[i];
+}
+return newStr;
+}
+console.log(disemvowel('This day super'));
+
+//#2
+function disemvowel(str) {
+  return str.match(/[^aeiou]/gi).join('');
+}
+
+//#3
+function disemvowel(str) {
+  let newStr = (str.replace(/A|E|I|O|U|a|e|i|o|u/g, ''));
+  return newStr;
+}
