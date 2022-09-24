b78cc35db2c208eb0c46100fd072ee8d4329dc3e
Богдан Атаманюк <bogdanatamanuk@MacBook-Air-Bogdan.local>
2022-09-24 09:47:43 +0200

    codewars duplicate other version

--- codewars/index.js
+++ codewars/index.js
@@ -217,3 +217,13 @@ function duplicateEncode(word) {
    .join('');
 }
 
+function duplicateEncode(word) {
+  const letterArray = word.toLowerCase().split("");
+  let outPutString = [];
+  for (i in letterArray) {
+    outPutString.push(
+      word.toLowerCase().split(letterArray[i]).length - 1 <= 1 ? "(" : ")"
+    );
+  }
+  return outPutString.join("");
+}
