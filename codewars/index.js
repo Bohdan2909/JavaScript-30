3a754a3c2be8f5d62c6d85b58e2007b46416832b
Богдан Атаманюк <bogdanatamanuk@MacBook-Air-Bogdan.local>
2022-09-24 10:25:42 +0200

    codewars array and human readable

--- codewars/index.js
+++ codewars/index.js
@@ -238,3 +238,62 @@ function nthFibo(n) {
   return n > 0 ? arr[n - 1] : 0;
 }
 console.log(nthFibo(4));//2
+
+//4 kyu Human readable duration format
+
+function formatDuration(seconds) {
+  if (seconds === 0) {
+    return 'now';
+  }
+  let typeArr = ['year', 'day', 'hour', 'minute', 'second'];
+  let sec = 0;
+  let minute = 0;
+  let hour = 0;
+  let day = 0;
+  let year = 0;
+  minute = Math.floor(seconds / 60);
+  sec = seconds % 60;
+
+  hour = Math.floor(minute / 60);
+  minute = minute % 60;
+
+  day = Math.floor(hour / 24);
+  hour = hour % 24;
+
+  year = Math.floor(day / 365);
+  day = day % 365;
+
+ let resultArr = [year, day, hour, minute, sec];
+ let resultStrArr = [];
+  for (let i = 0; i < resultArr.length; i++) {
+    if (resultArr[i] === 1) {
+      resultStrArr.push(resultArr[i] + ' ' + typeArr[i]);
+    } else if (resultArr[i] >= 2) {
+      resultStrArr.push(resultArr[i] + ' ' + typeArr[i] + 's');
+    }
+  }
+  let resultStr = '';
+  for (let i = 0; i < resultStrArr.length; i++) {
+    if (i === resultStrArr.length - 2) {
+      resultStr += resultStrArr[i] + ' and ' + resultStrArr[i + 1];
+      break;
+    }
+    if (i !== resultStrArr.length - 1) {
+      resultStr += resultStrArr[i] + ', ';
+    } else {
+      resultStr += resultStrArr[i];
+    }
+  }
+  return resultStr;
+}
+console.log(formatDuration(3645));
+
+//7 kyu Head, Tail, Init and Last
+
+const headEl = array => array[0];
+
+const tailEl = array => array.slice(1);
+
+const initEl = array => array.slice(0, -1);
+
+const lastEl = array => array[array.length - 1];
