7b0a5c4231a5bf98c0ac00a265ed26f36e38e84d
bogdanatamanuk <0997393654b@gmail.com>
2022-12-01 19:43:09 +0100

    codewars

--- codewars/index.js
+++ codewars/index.js
@@ -753,3 +753,13 @@ function findUniq(arr) {
   }
   const findUniq = arr => arr.sort()[0] === arr[1] ? arr.pop() : arr[0];
 console.log(findUniq([2, 2, 3, 2, 2, 2]));
+
+//6kyu Create Phone Number
+function createPhoneNumber(numbers){
+    let one = numbers.slice(0,3).join('')
+    let two = numbers.slice(3,6).join('')
+    let tree = numbers.slice(6,10).join('')
+    return `(${one}) ${two}-${tree}`
+  }
+  
+  console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))//(123) 456-7890
